f4345d2772f7cfa3a70f86d478c9b61d7accb78d
Agailh <agailham86@gmail.com>
2022-12-28 13:42:02 +0700

    wrapper and screen logic

---
+++ src/components/Screen.js
@@ -0,0 +1,12 @@
+import { Textfit } from "react-Textfit";
+import "./Screen.js";
+
+const Screen = ({ value }) => {
+  return (
+    <Textfit className="screen" mode="single" max={70}>
+      {value}
+    </Textfit>
+  );
+};
+
+export default Screen;
